af50df8b76affaf7eb9f4463c7fd0af94b750131
rmattson1008 <rmattson1008@gmail.com>
2022-02-15 12:45:39 -0500

    implement frame pairs dataset

---
+++ benchmarks/data_utils.py
@@ -0,0 +1,62 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import os
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+
+###
+# The dataset initializes with the file path to frame pairs
+# and loads the image when get image is called. Did this back when planning to take frames 
+# from entire video. Now the data is stored as as only 2 frames, so not really a necessary step
+###
+class FramePairDataset(Dataset):
+    def __init__(self, path_to_folder, class_types=['control', 'mdivi', 'llo'], transform=None):
+        self.labels= []
+        self.vid_path = []
+        self.transform = transform
+        # self.transform_input = transform_input
+        self.class_labels = class_types
+
+        print(path_to_folder)
+        
+
+        name_constraint = lambda x: 'normalized' in x
+        for label in self.class_labels:
+            path = os.path.join(path_to_folder, label)
+            files = os.listdir(path)
+            for file_name in files:
+                    if  name_constraint(file_name):
+                        self.vid_path.append(os.path.join(path, file_name))
+                        self.labels.append(label)
+
+    def __len__(self):
+        return len(self.labels)
+
+    
+    def __getitem__(self, idx):
+        # do we need to load the whole video to get the 2 frames
+        label = self.labels[idx]
+        vid = np.load(self.vid_path[idx])
+        first_frame = vid[0]
+        last_frame = vid[-1] 
+        # assert u loaded the right thing? eh
+
+        # ROI selection and such
+        if self.transform is not None:
+            first_frame = self.transform_image(first_frame)
+            last_frame = self.transform_image(last_frame)
+        
+
+        # To be discussed
+        # concept - should we take the difference, not the concatenation? 
+        # implement - should this be done in a torch.transform?
+        sample = np.concatenate((first_frame, last_frame))
+        sample = torch.from_numpy(sample)
+
+        return {"pair": sample, 'label': label}
+
+
+
