35455f4abf9b7f3f414a6440a9e9a01ef2d9dcb7
rmattson1008 <rmattson1008@gmail.com>
2022-03-13 19:05:20 -0400

    create parser for command line arguments

---
+++ benchmarks/parsing_utils.py
@@ -0,0 +1,100 @@
+from configargparse import ArgParser
+import os
+
+def make_parser(default_config_files=[".my_settings"]):
+    parser = ArgParser(description="Driver", default_config_files=default_config_files) #???? 
+
+    parser.add_argument(
+        '--weighted_sample', 
+        action='store_true',
+        help='choose to sample training data in a weighted manner',
+    )
+
+    parser.add_argument(
+        "--cuda",
+        type=int,
+        default=-1,
+        help="enables CUDA training on specified number of gpus (0 for cpu). Default to use all",
+    )
+
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=1,
+        metavar="b",
+        help="number of elements per minibatch",
+    )
+
+    parser.add_argument(
+        "--lr", type=float, default=1e-3, metavar="lr", help="learning rate"
+    )
+
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=10,
+        metavar="N",
+        help="number of epochs to train (default: 10)",
+    )
+
+    # TODO - list of class types
+    parser.add_argument(
+        "--classes",
+        type=list,
+        default=['control', 'mdivi', 'llo'],
+        metavar="cl",
+        help="list of possible class types",
+    )
+
+
+    parser.add_argument(
+        "--loss_fn",
+        type=str,
+        default="",
+        metavar="lf",
+        help="loss function",
+    )
+
+    #TODO - directory to get data 
+    parser.add_argument(
+        "--root_dir",
+        type=str,
+        default="./",
+        metavar="d",
+        help="sets the root directory for saving operations",
+    )
+
+    #TODO - directory to get data - this makes no sense.... just have it be absolute path
+    parser.add_argument(
+        "--input_dir",
+        type=str,
+        default="./",
+        metavar="i",
+        help="absolute path to the directory for accessing data",
+    ) 
+
+    #directory to specify where to save results
+    parser.add_argument(
+        "--out_dir",
+        type=str,
+        default="./",
+        metavar="o",
+        help="sets the directory for saving operations",
+    )
+
+    #Todo save model
+    parser.add_argument(
+            "--save",
+            type=str,
+            default="",
+            metavar="s",
+            help="saves the weights to a given filepath",
+        )
+
+    # What is this doing... and do I need it
+    # args.root_dir = args.root_path if args.root_path[-1] == "/" else f"{args.root_path}/" 
+    args = parser.parse_known_args()[0]
+    if args.save:
+        args.save = os.path.join(args.root_dir, args.save) # TODO - do u need to create folder?  
+
+    return args, parser
