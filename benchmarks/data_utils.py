b8e025f77f1dc8ff09b4bf85e572f725708742c1
rmattson1008 <rmattson1008@gmail.com>
2022-03-20 16:26:02 -0400

    implement transform that crops images to an ROI

--- benchmarks/data_utils.py
+++ benchmarks/data_utils.py
@@ -5,6 +5,10 @@ import os
 import torch
 from torch.utils.data import Dataset
 
+import torchvision
+import torchvision.transforms.functional as TF
+
+
 
 
 ###
@@ -46,15 +50,62 @@ class FramePairDataset(Dataset):
         first_frame = torch.as_tensor((first_frame))
         last_frame = torch.as_tensor((last_frame))
 
-
         # stack frames as channels, x shape is [2, img_width, img_height]
         sample = torch.stack((first_frame, last_frame))
 
-        # Does ROI go here?
+       
         if self.transform:
             sample = self.transform(sample)
             
         return sample, target_class
 
 
+#TODO is this batchable, I think it is
+# TODO, return some indication that image is empty. 
+    #TODO - if it ends up empty, did protein move or dissapear?? how much do we handle at the moment
+#TODO also this is kinda slow if that becomes a problem.
+class RoiTransform:
+    """Pick a ROI based on pixel intensity. Choose the same coordinates for both/all frames"""
 
+    def __init__(self, window_size=(28,28), kernel_size=5, use_first_frame_values=True):
+        """
+        window_size = (width, height) of cropped section
+        kernel_size = size of gaussian kernel used in selecting peak pixel values
+        use_first_frame_values = if true, set ROI based off first frame px values
+        """
+        self.width = window_size[0]
+        self.height = window_size[1]
+        self.kernel_size = kernel_size
+        self.use_first_frame = use_first_frame_values
+
+    def __call__(self, sample):
+        """
+        sample is [n_channels, img_width, img_height]
+        """
+
+        first_frame, last_frame = sample.split(1, dim=0)
+
+
+        if self.use_first_frame:
+            img_blurred = TF.gaussian_blur(first_frame, kernel_size=self.kernel_size) # TODO setting?
+        else:
+            img_blurred = TF.gaussian_blur(last_frame, kernel_size=self.kernel_size) # TODO setting?
+
+        values, indices = torch.topk(img_blurred.flatten(), k=1)
+        np_idx = np.array(np.unravel_index(indices.numpy(), img_blurred.shape))
+        # print("Top value", values)
+        # print("index", np_idx)
+
+        # self.index= 
+        if 0.0 in values:
+            print("NO DATA")
+
+        # width = 28
+        top = int(np_idx[1] - 1/2 * self.width)
+        left = int(np_idx[2] - 1/2 * self.height)
+        assert first_frame.shape == img_blurred.shape
+        # cropped = torchvision.transforms.functional.crop(x, top, left, self.width, self.height)
+        first_cropped = torchvision.transforms.functional.crop(first_frame, top, left, self.width, self.height)
+        last_cropped = torchvision.transforms.functional.crop(last_frame, top, left, self.width, self.height)
+
+        return (first_cropped, last_cropped)
