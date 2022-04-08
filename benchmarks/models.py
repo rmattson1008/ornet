5b3101023ff1689f280fbfc52b4c18e8ba32a5dd
rmattson1008 <rmattson1008@gmail.com>
2022-04-08 13:06:45 -0400

    rename base cnn model

--- benchmarks/models.py
+++ benchmarks/models.py
@@ -2,44 +2,8 @@ import torch
 from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Softmax, Module, BatchNorm2d
 
 
-class Model_0(Module):
-    
-
-    def __init__(self):
-        super().__init__()
-
-
-        self.cnn_layers = Sequential(
-            # Convolution 1
-            Conv2d(2,2,3,padding=1), 
-            BatchNorm2d(2),
-            ReLU(inplace=True),
-            MaxPool2d(2, stride=2),
-
-            # Convolution 2
-            #expand feature maps
-            Conv2d(2,4,3, padding=1), 
-            BatchNorm2d(4),
-            ReLU(inplace=True),
-            MaxPool2d(2, stride=2)
-        )
-
-        # should be length of unwound channels * feature map dims
-        db_size = 4 * 7 * 7
-        # one layer to classify
-        self.linear_layers = Sequential(Linear(db_size, 3))
-
-
-    def forward(self, x):
-        x = self.cnn_layers(x)
-        x = x.view(x.size(0), -1)
-        x = self.linear_layers(x)
-        return x
-
-
-
-# very simlar to the first, just expands feature maps immediately and has 2 linear layers
-class Model_1(Module):
+# 2 blocks, expands feature maps immediately and has 2 linear layers
+class BaseCNN(Module):
     
 
     def __init__(self):
@@ -74,6 +38,9 @@ class Model_1(Module):
         x = x.view(x.size(0), -1)
         x = self.linear_layers(x)
         return x
+
+
+
 
 # Losely styled after vgg model
 class VGG_Model(Module):
