31c2bf9cc94d1db8e8fd9628d7d29a03a3dca8f9
rmattson1008 <rachelmattson1008@gmail.com>
2022-04-21 11:59:06 -0400

    load features

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import pickle
 
 # for now just limiting by the final fricking representation files... 29,31,54.  
 
@@ -16,6 +17,18 @@ import numpy as np
 
 # Run a few classification models on all. like svm, random forest, etc
 # delany should do this. 
+
+# get deep features 
+with open('/home/rachel/representations/cnn/BaseCnn_embeddings10.pkl', 'rb') as f:
+            cnn_feature_dict = pickle.load(f)
+
+# print(feature_dict.keys())
+
+control_features = cnn_feature_dict["control"]
+mdivi_features = cnn_feature_dict["mdivi"]
+llo_features = cnn_feature_dict["llo"]
+
+
 
 to_data = "/data/ornet/gmm_intermediates"
 
