99fee367035c65f86fe5bb797adcd7050b677ff5
rmattson1008 <rachelmattson1008@gmail.com>
2022-04-28 09:16:26 -0400

    finish train and test

--- benchmarks/lstm_driver.py
+++ benchmarks/lstm_driver.py
@@ -10,9 +10,10 @@ from data_utils import DynamicVids, RoiTransform
 from models import CNN_LSTM
 from sklearn.metrics import confusion_matrix
 import numpy as np
-from parsing_utils import make_parser 
+from parsing_utils import make_parser
 
 from matplotlib import pyplot as plt
+
 
 def train(args, model, train_dataloader, val_dataloader, device='cpu'):
     lr = args.lr
@@ -21,21 +22,21 @@ def train(args, model, train_dataloader, val_dataloader, device='cpu'):
     optimizer = SGD(model.parameters(), lr=lr)
     criterion = CrossEntropyLoss()
 
-
     model.to(device)
-    for epoch in range(epochs):  
+    for epoch in range(epochs):
         print("Epoch", epoch)
         model.train()
         training_loss = 0.0
 
-        for data in train_dataloader: 
+        for i, data in enumerate(train_dataloader):
+            # if i % 30 == 0:
+
             # get the inputs; data is a list of [inputs, labels]
             # print(data.shape)
             inputs, labels = data[0].to(device), data[1].to(device)
-            print(labels)
-            inputs = inputs.float() # shouldn't stay on this step.
-            print("input shape", inputs.shape)
-         
+            # print(labels)
+            inputs = inputs.float()  # shouldn't stay on this step.
+            # print("input shape", inputs.shape)
 
             # zero the parameter gradients
             optimizer.zero_grad()
@@ -46,8 +47,61 @@ def train(args, model, train_dataloader, val_dataloader, device='cpu'):
             optimizer.step()
             training_loss += loss.item()
 
+        model.eval()
+        valid_loss = 0.0
+        for inputs, labels in val_dataloader:
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = inputs.to(device), labels.to(device)
+            inputs = inputs.float()  # shouldn't stay on this step.
+
+            logits, hidden_state = model(inputs)
+            # print(logits.shape)
+            _, pred = torch.max(logits, 1)
+            loss = criterion(logits, labels)
+            valid_loss += loss.item()
+
+        print(f'Epoch {epoch+1} \t\t Training Loss: {training_loss / len(train_dataloader) }\
+             \t\t Validation Loss: {valid_loss / len(val_dataloader )}')
+
+        # TODO - stop training when Val drops? val score is wack right now
+
+    if args.save:
+        print(args.save)
+        print("Saving model")
+        torch.save(model.state_dict(), args.save)
+
     return
 
+
+def test(args,  model, test_dataloader, show_plots=True, device='cpu'):
+    model.eval()  # is necessary?
+
+    with torch.no_grad():
+        y_true = torch.tensor([]).to(device)
+        y_pred = torch.tensor([]).to(device)
+        for images, labels in test_dataloader:
+            images, labels = images.to(device), labels.to(device)
+            images = images.float()
+            # calculate outputs by running images through the network
+            logits, _ = model(images)
+            # print(logits.shape)
+            _, predicted = torch.max(logits, 1)
+            # print(predicted.shape)
+            # bad approach?
+            y_pred = torch.cat((y_pred, predicted), 0)
+            y_true = torch.cat((y_true, labels), 0)
+
+    cm = confusion_matrix(y_true.cpu(), y_pred.cpu())
+
+    assert len(y_pred) == len(y_true)
+    accuracy = (y_true == y_pred).sum() / len(y_true)
+    print("Accuracy:", accuracy)
+
+    if show_plots:
+        print(args.classes)
+        print(cm)  # TODO - make pretty
+
+    return
 
 
 def get_dataloaders(args, display_images=False):
@@ -59,22 +113,24 @@ def get_dataloaders(args, display_images=False):
     #
     if args.roi:
         print("Using ROI inputs")
-        transform = transforms.Compose([RoiTransform(window_size=(28,28))])
+        transform = transforms.Compose([RoiTransform(window_size=(28, 28))])
     else:
         print("Using global image inputs")
         transform = transforms.Compose([transforms.Resize(size=28)])
-    dataset = DynamicVids(args.input_dir, class_types=args.classes, transform=transform) # nt working :/ 
+    dataset = DynamicVids(
+        args.input_dir, class_types=args.classes, transform=transform)  # nt working :/
 
     print("dataset", len(dataset))
 
-    ## don't think this is the way to do this... needs even percent split to work
+    # don't think this is the way to do this... needs even percent split to work
     train_split = .8
     train_size = int(train_split * len(dataset))
     test_size = int((len(dataset) - train_size) / 2)
     val_size = int((len(dataset) - train_size) / 2)
 
     # train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size], generator=torch.Generator().manual_seed(69))
-    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size], generator=torch.Generator().manual_seed(69))
+    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(
+        dataset, [train_size, val_size, test_size], generator=torch.Generator().manual_seed(69))
     test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size)
     val_dataloader = DataLoader(val_dataset, batch_size=args.batch_size)
 
@@ -87,14 +143,12 @@ def get_dataloaders(args, display_images=False):
     return train_dataloader, test_dataloader, val_dataloader
 
 
-
-
 if __name__ == "__main__":
     print("Main")
     # going to use the same parser manager for cnn and rnn
     args, _ = make_parser()
 
-    num_lstm_layers = 200 #too many?
+    num_lstm_layers = 200  # too many?
     # lstm_hidden_size = -1 # if we decide to have a different hidden space size then must add param to model
 
     model = CNN_LSTM(num_lstm_layers)
@@ -107,9 +161,18 @@ if __name__ == "__main__":
     if args.train:
         print("Training")
         train(args, model, train_dataloader, val_dataloader, device=device)
-    
-    # if args.test:
-    #     #TODO - if no model, load saved model
-    #     print("Testing")
-    #     test(args, model, test_dataloader)
 
+    if args.test:
+        if not args.train:
+            try:
+                saved_state = torch.load(args.save)
+                model.load_state_dict(saved_state)
+                # how to check soemthing happened..
+                print(type(model))
+            except:
+                # please exit
+                print(
+                    "Please provide the path to an existing model state using --save \"<path>\" or train a new one with --train")
+                exit()
+        print("Testing")
+        test(args, model, test_dataloader, device=device)
--- benchmarks/models.py
+++ benchmarks/models.py
@@ -3,74 +3,33 @@ from torch import nn
 from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Softmax, Module, BatchNorm2d
 
 
-class Model_0(Module):
-    
+# 2 blocks, expands feature maps immediately and has 2 linear layers
+class BaseCNN(Module):
 
     def __init__(self):
-        super(Model_0, self).__init__()
-
+        super().__init__()
 
         self.cnn_layers = Sequential(
             # Convolution 1
-            Conv2d(2,2,3,padding=1), 
-            BatchNorm2d(2),
-            ReLU(inplace=True),
-            MaxPool2d(2, stride=2),
-
-            # Convolution 2
-            #expand feature maps
-            Conv2d(2,4,3, padding=1), 
+            # expand feature maps
+            Conv2d(2, 4, 3),
             BatchNorm2d(4),
             ReLU(inplace=True),
-            MaxPool2d(2, stride=2)
+            MaxPool2d(3, stride=1),
+
+            # Convolution 2
+            Conv2d(4, 4, 3),
+            BatchNorm2d(4),
+            ReLU(inplace=True),
+            MaxPool2d(3, stride=1)
         )
 
         # should be length of unwound channels * feature map dims
-        db_size = 4 * 7 * 7
-        # one layer to classify
-        self.linear_layers = Sequential(Linear(db_size, 3))
-
-
-    def forward(self, x):
-        x = self.cnn_layers(x)
-        x = x.view(x.size(0), -1)
-        x = self.linear_layers(x)
-        return x
-
-
-import torch
-from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Softmax, Module, BatchNorm2d
-
-# very simlar to the first, just expands feature maps immediately and has 2 linear layers
-class Model_1(Module):
-    
-
-    def __init__(self):
-        super(Model_1, self).__init__()
-
-
-        self.cnn_layers = Sequential(
-            # Convolution 1
-            #expand feature maps
-            Conv2d(2,4,3,padding=1), 
-            BatchNorm2d(4),
-            ReLU(inplace=True),
-            MaxPool2d(2, stride=2),
-
-            # Convolution 2
-            Conv2d(4,4,3, padding=1), 
-            BatchNorm2d(4),
-            ReLU(inplace=True),
-            MaxPool2d(2, stride=2)
-        )
-
-        # should be length of unwound channels * feature map dims
-        db_size = 4 * 7 * 7
+        db_size = 4 * 20 * 20
         # one layer to classify
         self.linear_layers = Sequential(
             Linear(db_size, 10),
-            Linear(10,3))
-
+            Linear(10, 3))
 
     def forward(self, x):
         x = self.cnn_layers(x)
@@ -78,24 +37,23 @@ class Model_1(Module):
         x = self.linear_layers(x)
         return x
 
-# styled after vgg model
-class VGG_Model(Module):
-    
-    def __init__(self):
-        super(VGG_Model, self).__init__()
 
+class VGG_Model(Module):
+
+    def __init__(self):
+        super().__init__()
 
         self.cnn_layers = Sequential(
             # BLock 3
-            Conv2d(2,64,3,padding=1), 
+            Conv2d(2, 64, 3, padding=1),
             ReLU(inplace=True),
-            Conv2d(64,128,3,padding=1), 
+            Conv2d(64, 128, 3, padding=1),
             ReLU(inplace=True),
             MaxPool2d(2, stride=2),
             # Block 2
-            Conv2d(128,256,3,padding=1), 
+            Conv2d(128, 256, 3, padding=1),
             ReLU(inplace=True),
-            Conv2d(256,256,3,padding=1), 
+            Conv2d(256, 256, 3, padding=1),
             ReLU(inplace=True),
             MaxPool2d(2, stride=2),
         )
@@ -105,7 +63,6 @@ class VGG_Model(Module):
         # one layer to classify
         self.linear_layers = Sequential(Linear(db_size, 3))
 
-
     def forward(self, x):
         x = self.cnn_layers(x)
         x = x.view(x.size(0), -1)
@@ -114,7 +71,7 @@ class VGG_Model(Module):
 
 
 class CNN_LSTM(Module):
-    
+
     def __init__(self, num_lstm_layers, bidirectional=False):
         super(CNN_LSTM, self).__init__()
         # self.lstm_hidden_size = lstm_hidden_size
@@ -122,42 +79,37 @@ class CNN_LSTM(Module):
         self.bidirectional = bidirectional
 
         self.cnn_layers = Sequential(
-            Conv2d(1,4,3,padding=1), 
+            Conv2d(1, 4, 3, padding=1),
             BatchNorm2d(4),
             ReLU(inplace=True),
             MaxPool2d(2, stride=2),
             # Convolution 2
-            Conv2d(4,4,3, padding=1), 
+            Conv2d(4, 4, 3, padding=1),
             BatchNorm2d(4),
             ReLU(inplace=True),
             MaxPool2d(2, stride=2)
-            )
+        )
         cnn_representation_size = 4 * 7 * 7
-        
 
         self.lstm_input_size = cnn_representation_size
         self.lstm_hidden_size = cnn_representation_size
 
-        self.lstm = nn.LSTM(input_size=self.lstm_input_size, hidden_size=self.lstm_hidden_size, num_layers=self.num_lstm_layers, bidirectional=self.bidirectional)
-        self.linear_layers = Sequential(Linear(self.lstm_hidden_size, 3)) # Really??? 
-
-
-        
+        self.lstm = nn.LSTM(input_size=self.lstm_input_size, hidden_size=self.lstm_hidden_size,
+                            num_layers=self.num_lstm_layers, bidirectional=self.bidirectional)
+        self.linear_layers = Sequential(Linear(self.lstm_hidden_size, 3))
 
     # def forward(self, x, prev_state):
+
     def forward(self, x):
-        
-        hidden_state = None # maybe actually init it, or use previous
+
+        hidden_state = None  # maybe actually init it, or use previous
         for t in range(x.size(1)):
-            # with torch.no_grad(): # i think we want to unfreeze the cnn. everyone else doing this uses pretrained cnn oh well. 
-            frames = self.cnn_layers(x[:,t, :, :, :])  
+            # with torch.no_grad(): # i think we want to unfreeze the cnn. everyone else doing this uses pretrained cnn oh well.
+            frames = self.cnn_layers(x[:, t, :, :, :])
             frames = torch.flatten(frames, start_dim=1)
-            out, hidden_state = self.lstm(frames, hidden_state)  
+            out, hidden_state = self.lstm(frames, hidden_state)
             # we are not saving the sequence of hidden states for one sample
             # nor are we sharing hidden states over batches
 
         logits = self.linear_layers(out)
         return logits, hidden_state
-
-
-
