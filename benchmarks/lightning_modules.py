a15e0ca660fedd7f849c8dc01abb11e8b3a02d79
rmattson1008 <rachelmattson1008@gmail.com>
2023-01-13 15:25:00 -0500

    add lstm as submodule to lightning module (wip)

--- benchmarks/lightning_modules.py
+++ benchmarks/lightning_modules.py
@@ -21,11 +21,6 @@ import logging
 from sklearn.mixture import GaussianMixture as GMM
 
 #please please please clean. 
-    
-def get_features(name):
-    def hook(model, input, output):
-        features[name] = output.detach()
-    return hook
 
 class DataAugmentation(nn.Module):
     """Module to perform data augmentation using Kornia on torch tensors."""
@@ -157,13 +152,9 @@ class CNN_Module(pl.LightningModule):
 
         frames = frames.permute((1,0,2))
         # aggregate frames
-        fcs_vector = self.aggregator(frames, start_dim=1)
+        aggregated_frames = self.aggregator(frames, start_dim=1)
 
-    
-        # print("flat shape" ,fcs_vector.shape)
-
-        # print("")
-        logits = self.linear_layers(fcs_vector)
+        logits = self.linear_layers(aggregated_frames)
         return logits
 
     def configure_optimizers(self):
@@ -391,308 +382,45 @@ class CNN_Module(pl.LightningModule):
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class LSTM(nn.Module):
-    def __init__(self, number_of_frames=5, num_classes=2, learning_rate=0.00001, weight_decay=0, label="model", dropout=False):
+    def __init__(self, number_of_frames=5, num_classes=2, learning_rate=0.00001, weight_decay=0, label="lstm_model"):
         super().__init__()
         self.register_buffer("sigma", torch.eye(3))
         self.number_of_frames = number_of_frames
         self.num_classes= num_classes
         self.out_height = 128
-        self.train_accuracy = torchmetrics.Accuracy()
-        self.val_accuracy = torchmetrics.Accuracy()
-        self.test_accuracy = torchmetrics.Accuracy()
         self.label = label
-        self.transform = DataAugmentation()
         self.hidden_layer_size = 128
-        self.dropout = dropout
-        self.show_image = True
-        # self.val_bin_accuracy = torchmetrics.BinaryAccuracy()
 
-        self.lr = learning_rate
-        print("using learning rate ", self.lr)
-        self.wd = weight_decay
-
-        # self.save_hyperparameters()
-        # self.device = device
-
-        # resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
-        # resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
-        squeeze = torch.hub.load('pytorch/vision:v0.10.0', 'squeezenet1_0', pretrained=False)
-        print(squeeze)
-        # resnet.conv1 = Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-        squeeze.features[0] = Conv2d(1, 96, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-        # resnet.fc = torch.nn.Linear(2048, self.out_height,bias=.6)
-        # resnet.fc = torch.nn.Linear(512, self.out_height)
-        # squeeze.classifier = torch.nn.Linear(512, self.out_height)
-        self.fc = torch.nn.Linear(1000, self.out_height)
-        if self.dropout:
-            # resnet.avgpool.register_forward_hook(lambda m, inp, out: F.dropout(out, p=0.5, training=m.training))
-            # resnet.fc.register_forward_hook(lambda m, inp, out: F.dropout(out, p=0.5, training=m.training))
-
-            squeeze.classifier.register_forward_hook(lambda m, inp, out: F.dropout(out, p=0.5, training=m.training))
-        print(squeeze)
-
-        # self.cnn_layers = resnet
-        self.cnn_layers = squeeze
+        # self.lr = learning_rate
+        # self.wd = weight_decay
         self.lstm = nn.LSTM(self.out_height,self.hidden_layer_size, 1)
 
-        # self.flattened_frames_size = self.number_of_frames * 1 * self.out_height.... what is out size?
-        self.linear_layers = torch.nn.Sequential(torch.nn.Linear(self.hidden_layer_size * self.number_of_frames, 10), torch.nn.Linear(10, self.num_classes) )
-        if self.dropout:
-            # pass
-            print('Using dropout')
-            self.linear_layers[0].register_forward_hook(lambda m, inp, out: F.dropout(out, p=0.25, training=m.training))
-       
-    def forward(self, x):
-        # I want to classify like 3-5 frames
-        frames = torch.empty((x.size(1), x.size(0) , self.out_height)).type_as(x)
+    
+    def forward(self, frames):
+        print(frames.shape)
 
-        # This chunk is pretty gross, should just keep dummy channel dim
-        # print(x.shape)
-        # self.logger.experiment.add_image("train image before transform", x[0][0].unsqueeze(dim=0))
-        x = x.unsqueeze(dim=1)
-        # print(x.shape)
-        x = self.transform(x)
-        # print(x.shape)
-        x = x.squeeze(dim=1)
-
-        # self.logger.experiment.add_image("train image", x[0][0].unsqueeze(dim=0))
-        if self.show_image:
-            # print("printing")
-            image_to_print = x[0][0].clone().detach().unsqueeze(dim=0)
-            self.logger.experiment.add_image("train image before transform",image_to_print )
-            self.show_image = False
-        for t in range(self.number_of_frames):
-            # with torch.no_grad(): # i think we want to unfreeze the cnn. everyone else doing this uses pretrained cnn oh well.
-            # print(x[:, t, :, :].unsqueeze(1).shape)
-            frames[t] =  self.fc(self.cnn_layers(x[:, t, :, :].unsqueeze(1)))
-
-        frames = frames.flatten(start_dim=2)
+        frames = frames.flatten(start_dim=2) #flattening the 2d image into one vector
 
         #todo, switch if bi
         h0 = torch.randn(1, frames.size(1), self.hidden_layer_size).type_as(x)
         c0 = torch.randn(1,frames.size(1), self.hidden_layer_size).type_as(x)
         out, (hn, cn) = self.lstm(frames)
-        out = out.permute((1,0,2))
+        out = out.permute((1,0,2))#why??
+        print(out.shape)
 
-        out = out.flatten(start_dim=1)
-        # print(out.shape)
-
-        logits = self.linear_layers(out)
+        out = out.flatten(start_dim=1) #why? do I really need this?
+        print(out.shape)
         # print('applied classifier')
-        return logits
+        # should be one point in space from 3
+        return out
 
 
+"""keep in mind a LightningModule is a nn.Module, so whenever you define a nn.Module as 
+attribute to a LightningModule in the __init__ function, this module will end being registered
+ as a sub-module to the parent pytorch lightning module."""
 
 
-
-
-
-# class CnnLSTM_Module(pl.LightningModule):
-#     def __init__(self, number_of_frames=5, num_classes=2, learning_rate=0.00001, weight_decay=0, label="model", dropout=False):
-#         super().__init__()
-#         self.register_buffer("sigma", torch.eye(3))
-#         self.number_of_frames = number_of_frames
-#         self.num_classes= num_classes
-#         self.out_height = 128
-#         self.train_accuracy = torchmetrics.Accuracy()
-#         self.val_accuracy = torchmetrics.Accuracy()
-#         self.test_accuracy = torchmetrics.Accuracy()
-#         self.label = label
-#         self.transform = DataAugmentation()
-#         self.hidden_layer_size = 128
-#         self.dropout = dropout
-#         self.show_image = True
-#         # self.val_bin_accuracy = torchmetrics.BinaryAccuracy()
-
-#         self.lr = learning_rate
-#         print("using learning rate ", self.lr)
-#         self.wd = weight_decay
-
-#         # self.save_hyperparameters()
-#         # self.device = device
-
-#         # resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
-#         # resnet = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
-#         squeeze = torch.hub.load('pytorch/vision:v0.10.0', 'squeezenet1_0', pretrained=False)
-#         print(squeeze)
-#         # resnet.conv1 = Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-#         squeeze.features[0] = Conv2d(1, 96, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-#         # resnet.fc = torch.nn.Linear(2048, self.out_height,bias=.6)
-#         # resnet.fc = torch.nn.Linear(512, self.out_height)
-#         # squeeze.classifier = torch.nn.Linear(512, self.out_height)
-#         self.fc = torch.nn.Linear(1000, self.out_height)
-#         if self.dropout:
-#             # resnet.avgpool.register_forward_hook(lambda m, inp, out: F.dropout(out, p=0.5, training=m.training))
-#             # resnet.fc.register_forward_hook(lambda m, inp, out: F.dropout(out, p=0.5, training=m.training))
-#             squeeze.classifier.register_forward_hook(lambda m, inp, out: F.dropout(out, p=0.5, training=m.training))
-#         print(squeeze)
-
-#         # self.cnn_layers = resnet
-#         self.cnn_layers = squeeze
-#         self.lstm = nn.LSTM(self.out_height,self.hidden_layer_size, 1)
-
-#         # self.flattened_frames_size = self.number_of_frames * 1 * self.out_height.... what is out size?
-#         self.linear_layers = torch.nn.Sequential(torch.nn.Linear(self.hidden_layer_size * self.number_of_frames, 10), torch.nn.Linear(10, self.num_classes) )
-#         if self.dropout:
-#             # pass
-#             print('Using dropout')
-#             self.linear_layers[0].register_forward_hook(lambda m, inp, out: F.dropout(out, p=0.25, training=m.training))
-       
-
-#     def forward(self, x):
-#         # I want to classify like 3-5 frames
-#         frames = torch.empty((x.size(1), x.size(0) , self.out_height)).type_as(x)
-
-#         # This chunk is pretty gross, should just keep dummy channel dim
-#         # print(x.shape)
-#         # self.logger.experiment.add_image("train image before transform", x[0][0].unsqueeze(dim=0))
-#         x = x.unsqueeze(dim=1)
-#         # print(x.shape)
-#         x = self.transform(x)
-#         # print(x.shape)
-#         x = x.squeeze(dim=1)
-
-#         # self.logger.experiment.add_image("train image", x[0][0].unsqueeze(dim=0))
-#         if self.show_image:
-#             # print("printing")
-#             image_to_print = x[0][0].clone().detach().unsqueeze(dim=0)
-#             self.logger.experiment.add_image("train image before transform",image_to_print )
-#             self.show_image = False
-#         for t in range(self.number_of_frames):
-#             # with torch.no_grad(): # i think we want to unfreeze the cnn. everyone else doing this uses pretrained cnn oh well.
-#             # print(x[:, t, :, :].unsqueeze(1).shape)
-#             frames[t] =  self.fc(self.cnn_layers(x[:, t, :, :].unsqueeze(1)))
-
-#         frames = frames.flatten(start_dim=2)
-
-#         #todo, switch if bi
-#         h0 = torch.randn(1, frames.size(1), self.hidden_layer_size).type_as(x)
-#         c0 = torch.randn(1,frames.size(1), self.hidden_layer_size).type_as(x)
-#         out, (hn, cn) = self.lstm(frames)
-#         out = out.permute((1,0,2))
-
-#         out = out.flatten(start_dim=1)
-#         # print(out.shape)
-
-#         logits = self.linear_layers(out)
-#         # print('applied classifier')
-#         return logits
-
-#     def configure_optimizers(self):
-#         optimizer = torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.wd)
-#         #TODO- use my args
-#         return optimizer
-
-#     def training_step(self, batch, batch_idx):
-#         inputs, labels = batch
-#         # print(inputs.shape)
-#         outputs = self(inputs)
-#         preds = outputs.softmax(dim=-1)
-
-#         loss = F.cross_entropy(outputs, labels)
-        
-#         return {'loss': loss, 'pred': preds, 'target': labels}
-
-
-#     def training_step_end(self, outputs):
-#         self.train_accuracy(outputs['pred'], outputs['target'])
-#         self.log('train_accuracy', self.train_accuracy)
-#         self.log('train_loss', outputs['loss'], sync_dist=True)
-#         return outputs
-
-
-#     def validation_step(self, batch, batch_idx):
-#         x, y = batch
-#         y_hat = self(x)
-#         loss = F.cross_entropy(y_hat, y)
-#         # self.log('validation_loss', val_loss, on_step=True, on_epoch=True, sync_dist=True)
-#         pred = y_hat.softmax(dim=-1)
-#         # print("val preds", preds)
-        
-#         return {'loss':loss, 'pred': pred, 'target': y}
-#         # return pred
-
-#     def validation_step_end(self, outputs):
-#         self.val_accuracy(outputs['pred'], outputs['target'])
-#         self.log('val_accuracy', self.val_accuracy)
-#         self.log('val_loss', outputs['loss'], sync_dist=True)
-
-#     def test_step(self, batch, batch_idx):
-#         x, verbose_y = batch
-#         y = verbose_y['label']
-#         print("my y", y)
-
-#         y_hat = self(x)
-#         loss = F.cross_entropy(y_hat, y)
-#         # self.log('validation_loss', val_loss, on_step=True, on_epoch=True, sync_dist=True)
-#         pred = y_hat.softmax(dim=-1)
-#         # print("val preds", preds)
-
-#         #if wrong, print video and timestamp to buffer. 
-
-
-#         self.test_accuracy(pred, y)
-#         return {'loss':loss, 'pred': pred, 'target': y, 'embed': y_hat}
-#         # return pred
-    
-#     def test_epoch_end(self, outputs):
-       
-#         embeds = [x['embed'].cpu().to('cpu') for x in outputs] 
-#         targets = [x['target'].cpu().to('cpu') for x in outputs] 
-#         preds = [x['pred'].cpu().to('cpu') for x in outputs] 
-      
-#         plot = get_embedding_plot(embeds, targets, preds, self.label)
-
-
-#         # print("saving embeddings")
-#         # with open("embeddings/" + self.label +"_embeddings.pkl", "wb") as fp:   #Pickling
-#         #     pickle.dump(embeds, fp)
-#         # with open("embeddings/" + self.label +"_targets.pkl", "wb") as fp:   #Pickling
-#         #     pickle.dump(targets, fp)
-#         # with open("embeddings/" + self.label +"_preds.pkl", "wb") as fp:   #Pickling
-#             # pickle.dump(preds, fp)
-#         # self.test_accuracy(outputs['pred'], outputs['target'])
-
-
-#         self.logger.experiment.add_figure("test embedding", plot)
-#         print('test_accuracy', self.test_accuracy.compute())
-#         return
 
 
 
