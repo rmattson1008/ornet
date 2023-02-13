a87344c85a5ffeaad6b3c30d1ccee8d31f0da666
rmattson1008 <rachelmattson1008@gmail.com>
2023-02-13 12:36:15 -0500

    compute gmm only in test

--- benchmarks/lightning_modules.py
+++ benchmarks/lightning_modules.py
@@ -23,7 +23,9 @@ import logging
 from sklearn.mixture import GaussianMixture as GMM
 from torch.nn import LSTM
 
-#please please please clean. 
+
+from torchmetrics.classification import Accuracy
+
 
 features = {}
 
@@ -44,13 +46,9 @@ class DataAugmentation(nn.Module):
         self.transforms = K.VideoSequential(
             # K.Normalize([0.0024, 0.0024, 0.0024], [0.0149, 0.0149, 0.0150], p=1.0),
             K.Normalize([0.0024, 0.0024, 0.0024, 0.0024, 0.0024], [0.0149, 0.0149, 0.0150, 0.0150, 0.0150], p=1.0),
-            # ColorJiggle(0.1, 0.1, 0.1, 0.1, p=.5),
             RandomHorizontalFlip(p=0.5),
             RandomVerticalFlip(p=0.5),
-            # RandomChannelShuffle(p=0.75),
-            # RandomThinPlateSpline(p=0.5),
-            # RandomGaussianNoise
-            # RandomPlasmaShadow
+
         )
         self.jitter = ColorJitter(0.5, 0.5, 0.5, 0.5)
 
@@ -79,9 +77,9 @@ class CNN_Module(pl.LightningModule):
         self.dropout = dropout
         self.show_image = False
         self.gmm = None
+
         assert aggregator in ['flatten', 'lstm', 'mean']
         self.aggregator_type = aggregator
-
         
     
         self.lr = learning_rate
@@ -106,12 +104,6 @@ class CNN_Module(pl.LightningModule):
 
 
         self.linear_layers = torch.nn.Sequential(torch.nn.Linear(self.flattened_frames_size, self.num_classes))
-  
-        # self.fc = torch.nn.Sequential(torch.nn.Linear(1000, self.out_height), torch.nn.Linear(self.out_height, self.out_height))
-        # self.linear_layers = torch.nn.Sequential(torch.nn.Linear(self.flattened_frames_size, 10), torch.nn.Linear(10, self.num_classes) )
-        # self.linear_layers = torch.nn.Sequential(torch.nn.Linear(self.flattened_frames_size, 64),torch.nn.Linear(64, 32) , torch.nn.Linear(32, 16), torch.nn.Linear(16, self.num_classes))
-        # self.linear_layers.register_forward_hook(lambda m, inp, out: F.dropout(out, p=0.5, training=m.training))
-        # self.alt_linear_layer
 
         self.hook = self.linear_layers.register_forward_hook(get_features('feats'))
         if self.dropout:
@@ -122,15 +114,12 @@ class CNN_Module(pl.LightningModule):
  
 
     def forward(self, x):
-        #is this on the comp. graph
+        #is this all on the comp. graph
         frames = torch.empty((x.size(1), x.size(0), self.out_height)).type_as(x)
 
         x = x.unsqueeze(dim=1)
-        # print(x.shape)
         x = self.transform(x)
-        # print(x.shape)
         x = x.squeeze(dim=1)
-        # print(x.shape)
 
         if self.show_image:
             image_to_print = x[0][0].clone().detach().unsqueeze(dim=0)
@@ -141,7 +130,6 @@ class CNN_Module(pl.LightningModule):
             frames[t] = self.fc(self.cnn_layers(x[:, t, :, :].unsqueeze(1)))
         frames = frames.permute((1,0,2)) # restore batch order
 
-
         if self.aggregator_type == 'flatten': 
             aggregated_frames = self.aggregator(frames, start_dim=1)
             #TODO- how do I do fancy lambdas here.
@@ -149,8 +137,9 @@ class CNN_Module(pl.LightningModule):
         if self.aggregator_type == 'lstm':
             outputs, (hn, cn) = self.aggregator(frames)
             aggregated_frames = hn[-1]
-    
 
+        # if self.aggregator_type == 'mean':
+        #    aggregated_frames  = self.aggregator(frames)
 
         logits = self.linear_layers(aggregated_frames)
         return logits
@@ -160,34 +149,22 @@ class CNN_Module(pl.LightningModule):
         return optimizer
 
     
-    def get_embedding_plot(self, embeddings, targets, preds, name):
+    def get_embedding_plot(self, embeddings, labels, preds, name):
         """
         Get the an embedding from the final embedding (the decision space).
         """
-        print("getting plot...")
+        # print("getting plot...")
         xs = []
         ys = []
 
         labels = []
         preds = []
 
-        # points = []
 
         points = self.batch2points(embeddings)
         temp = np.transpose(points)
         xs = temp[0]
         ys = temp[1]
-
-        print("X shape", xs.shape)
-        
-        # TODO - use batch2points and detach correctly
-        for batch in targets:
-            labels.extend(batch.numpy())
-
-        
-        # preds = self.batch2points(preds)
-        # or reshape
-        # preds = [x.argmax(axis=0) for x in preds]
 
         for batch in preds:
             for encoding in batch: 
@@ -199,7 +176,6 @@ class CNN_Module(pl.LightningModule):
         np.random.seed(42)
         fig = plt.figure()
         ax = fig.add_subplot()
-        # plt.plot(xs, ys, c=labels, cmap=matplotlib.colors.ListedColormap(colors))
         ax.scatter(xs, ys, c=labels, cmap=matplotlib.colors.ListedColormap(colors))
     
         plt.title(name)
@@ -216,31 +192,28 @@ class CNN_Module(pl.LightningModule):
          """
         
         # never do it like this
-        # I think the pythonic way is to use reshape(-1)??
+        # I think the pythonic way is to use reshape()??
         # please redo before leaving this project
+        # this comment will probably live forever in the project
 
         points = []   
         for batch in l:
             points.extend(batch)
         points = np.stack(points)
 
-        print("batch2points returned an array of shape", points.shape)
+        # print("batch2points returned an array of shape", points.shape)
         return points
 
     def get_gmm_memberships(self, points: np.ndarray):
         print("entering the call to gmm")
-        # print(points.shape)
-        # print(points[0].shape)
         membership_probs = self.gmm.predict_proba(points)
         print(membership_probs.shape)
         return membership_probs
 
     def get_gmm_preds(self, points:np.ndarray):
-        print("points", points.shape)
+        print("entering get gmm preds")
         membership_probs = self.gmm.predict_proba(points)
-        print("Test", membership_probs.shape)
-        preds =  membership_probs.argmax(axis=1)
-        print(preds.shape)
+        preds = membership_probs.argmax(axis=1)
         return preds
 
     def get_gmm_imprint(self, n=100):   
@@ -251,36 +224,24 @@ class CNN_Module(pl.LightningModule):
         xxs = [x[0] for x in samples]
         yys = [x[1] for x in samples]
         plt.scatter(xxs, yys, c=labels)
+
+        # xxs = [x[2] for x in samples]
+        # yys = [x[3] for x in samples]
+        # plt.scatter(xxs, yys, c=labels)
+
+        # xxs = [x[4] for x in samples]
+        # yys = [x[5] for x in samples]
+        # plt.scatter(xxs, yys, c=labels)
+
+        # xxs = [x[6] for x in samples]
+        # yys = [x[7] for x in samples]
+        # plt.scatter(xxs, yys, c=labels)
+
         return plt.gcf()
 
-    # def get_gmm_diff_plot(self, points):
-    #     """
-    #     So this is probably where I want to see where test videos or control videos fit in to the mm memberships. 
-    #     It would be nice to project the memberships between -1 and 1, and plot how those change over time. 
-    #     Not sure that the test dataset is ready for this tho...
-    #     Got to review gmm outputs...
-    #     I guess to start I could simply plot the membership on two axes
-    #     Then switch to a view over time.
-    # This code doesnt make any sennse
-    #     """
-
-    #     membership_probs = self.get_gmm_memberships(points)
-
-    #     prob_diff = [p[0] - p[1] for p in membership_probs[:20]]
-
-    #     fig = plt.figure()
-    #     ax = fig.add_subplot()
-    #     ax.scatter( range(0,len(prob_diff)), prob_diff)
-    #     ax.plot(range(0,len(prob_diff)), prob_diff)
-
-    #     plt.title("GMM membership")
-    #     ax.set_ylabel('membership prob diff')
-    #     ax.set_xlabel('time stamp')
-    #     return plt.gcf()
 
     def training_step(self, batch, batch_idx):
         inputs, labels = batch
-        # print(inputs.shape)
         outputs = self(inputs)
         preds = outputs.softmax(dim=-1)
         loss = F.cross_entropy(outputs, labels)
@@ -294,64 +255,45 @@ class CNN_Module(pl.LightningModule):
 
     def training_epoch_end(self, outputs):
     
-        embeds = [x['embeds'].detach().cpu().numpy() for x in outputs]
+        # embeds = [x['embeds'].detach().cpu().numpy() for x in outputs]
         # get features
         # Zs = [x['z'].detach().cpu().numpy().reshape(-1) for x in outputs]
-        Zs = [x['z'].detach().cpu().numpy() for x in outputs]
+        # Zs = [x['z'].detach().cpu().numpy() for x in outputs]
 
-        points = self.batch2points(Zs) 
+        # points = self.batch2points(Zs) 
 
         # so ur getting a new model every time.
-        print("right before fit:", points[0].shape, len(points), " samples")
-        self.gmm = GMM(n_components=2, random_state=0).fit(points)
-        print("GMM fit metrics")
-        print("bic" , self.gmm.bic(points))
-        print("aic", self.gmm.aic(points))
-        sample_plot = self.get_gmm_imprint()
-        self.logger.experiment.add_figure("gmm sample", sample_plot)
-        self.log('gmm bic', self.gmm.bic(points),sync_dist=True)
+        # print("right before fit:", points[0].shape, len(points), " samples")
+        # self.gmm = GMM(n_components=2, random_state=0).fit(points)
+        # print("GMM fit metrics")
+        # print("bic" , self.gmm.bic(points))
+        # print("aic", self.gmm.aic(points))
+        # sample_plot = self.get_gmm_imprint()
+        # self.logger.experiment.add_figure("gmm sample", sample_plot)
+        # self.log('gmm bic', self.gmm.bic(points),sync_dist=True)
+        return
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         logits = self(x)
         loss = F.cross_entropy(logits, y) #???
-        # self.log('validation_loss', val_loss, on_step=True, on_epoch=True, sync_dist=True)
         pred = logits.softmax(dim=-1)
-        # print("val preds", preds)
-
-        if self.gmm is not None:
-            Zs = features['feats'].cpu().numpy()
-            gmm_preds = self.get_gmm_preds(Zs)
-            print(type(gmm_preds))
-
-            self.gmm_accuracy(torch.Tensor(gmm_preds), y.detach().cpu()) # dont' use this 
-        
         return {'loss':loss, 'pred': pred, 'target': y, 'embeds':logits}
         # return pred
 
     def validation_step_end(self, outputs):
         self.val_accuracy(outputs['pred'], outputs['target'])
         self.log('val_accuracy', self.val_accuracy)
-        
         self.log('val_loss', outputs['loss'], sync_dist=True)
         return outputs
 
     def validation_epoch_end(self, outputs):
-            #lets see what happens if we do this every time
-        #I think this is a bad idea why not use detach? 
         embeds = [x['embeds'].detach().cpu().numpy() for x in outputs]
         targets = [x['target'].cpu().to('cpu') for x in outputs] 
         preds = [x['pred'].cpu().to('cpu') for x in outputs] 
 
-        if self.gmm is not None:
-            print("now computing accuracy")
-           
-            acc = self.gmm_accuracy.compute()
-            self.log('gmm_accuracy', acc)
-
-            print("Now plotting")
-            plot = self.get_embedding_plot(embeds, targets, preds, self.label)
-            self.logger.experiment.add_figure("val embedding", plot)
+        plot = self.get_embedding_plot(embeds, targets, preds, self.label)
+        self.logger.experiment.add_figure("val embedding", plot)
         return
 
     def test_step(self, batch, batch_idx):
@@ -367,36 +309,36 @@ class CNN_Module(pl.LightningModule):
     
     def test_epoch_end(self, outputs):
        
+       # I truly don't know how to deal with this data type. this is the only way??
         embeds = [x['embeds'].detach().cpu().numpy() for x in outputs]
         Zs = [x['z']['feats'].detach().cpu().numpy() for x in outputs]
 
-        targets = [x['target'].cpu().to('cpu') for x in outputs] 
+        targets = [x['target'].cpu().to('cpu').numpy() for x in outputs] 
         preds = [x['pred'].cpu().to('cpu') for x in outputs] 
     
-        print("Features", len(Zs))
-        print("Features", Zs[0].shape)
-        Zs = self.batch2points(Zs)
+        points = self.batch2points(Zs)
 
+        print("shape of  one sample right before fit:", points[0].shape, len(points), " samples")
+        self.gmm = GMM(n_components=2, random_state=0).fit(points)
+        gmm_preds = self.get_gmm_preds(points) # jesus 
+     
         with open('features_out.npy', 'wb') as f:
             np.save(f, Zs)
 
-        plot = self.get_embedding_plot(embeds, targets, preds, self.label)
+        print("plotting...")
+        labels = targets
+        plot = self.get_embedding_plot(embeds, labels, preds, self.label)
         self.logger.experiment.add_figure("test embedding", plot)
+
+        print("gmm accuracy...")
+        # by hand
+        metric = Accuracy()
+        gmm_acc = metric(torch.Tensor(gmm_preds), torch.tensor(targets).squeeze(dim=-1)) 
+        self.log('gmm_accuracy', gmm_acc)
 
         acc = self.test_accuracy.compute()
         print('test_accuracy', acc)
         self.log('test accuracy', acc)
+        
         return
-
-
-
-
-"""keep in mind a LightningModule is a nn.Module, so whenever you define a nn.Module as 
-attribute to a LightningModule in the __init__ function, this module will end being registered
- as a sub-module to the parent pytorch lightning module."""
-
-
-
-#TODO - now the imprints wont work
-
 
