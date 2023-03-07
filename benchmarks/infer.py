a9ca273d470cfa4655e26ed39bb5931892eb0093
rmattson1008 <rachelmattson1008@gmail.com>
2023-03-07 16:23:10 -0500

    clean work

--- benchmarks/infer.py
+++ benchmarks/infer.py
@@ -32,7 +32,7 @@ import numpy as np
 from parsing_utils import make_parser
 from sklearn.model_selection import train_test_split
 
-# from lightning_driver import get_dataloaders
+from lightning_driver import get_dataloaders
 
 class Dict2Class(object):
       
@@ -40,34 +40,6 @@ class Dict2Class(object):
           
         for key in my_dict:
             setattr(self, key, my_dict[key])
-
-
-def get_dataloaders(args,time_steps=3, frames_per_chunk=3, resize=224):
-    """
-    Access ornet dataset, pass any initial transformations to dataset,
-    split into train/test/validate, and return dataloaders
-    """
-    X, y = get_accept_list("/data/ornet/gmm_intermediates", ['control', 'mdivi', 'llo'])
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
-    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=39)
-
-    transform = transforms.Compose([transforms.ToTensor(),transforms.Resize(size=resize)])
-    # TODO - some sort of normalizing step?
-    # transform = transforms.Compose([ transforms.ToTensor(), transforms.Resize(size=224), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-    print("creating train dataset")
-    train_dataset = TimeChunks(args.input_dir, accept_list=X_train, frames_per_chunk=frames_per_chunk, step=args.step, transform=transform)  
-    print("creating val dataset")
-    val_dataset = TimeChunks(args.input_dir, accept_list=X_val, frames_per_chunk=frames_per_chunk,step=args.step,   transform=transform)  
-    
-    print("creating test dataset")
-    test_dataset = TimeChunks(args.input_dir, accept_list=X_test, frames_per_chunk=frames_per_chunk, step=args.step,  transform=transform, verbose=True, shuffle_chunks=False)  
-
-    train_dataloader = DataLoader(train_dataset,shuffle=args.shuffle, sampler=None, batch_size=args.batch_size, num_workers=4)
-    test_dataloader = DataLoader(test_dataset,shuffle=args.shuffle,sampler=None, batch_size=1, num_workers=4)
-    val_dataloader = DataLoader(val_dataset,shuffle=args.shuffle,sampler=None, batch_size=args.batch_size, num_workers=4)
-
-    return train_dataloader, test_dataloader, val_dataloader
-
 
 
 
